cb3f40ee75a191b049a535700fa144a827228074
maryam najafi <maryam.najafi733@.com>
2025-12-23 00:16:29 +0330

    admin edit product added

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request,redirect,session,abort
+from flask import Blueprint,render_template,request,redirect,session,abort,url_for
 import config
 from models.product import Product
 from extentions import db
@@ -52,3 +52,32 @@ def products():
           db.session.add(p)
           db.session.commit()
           return 'done'
+
+@app.route('/admin/dashboard/edit-product/<id>',methods=["GET","POST"])
+
+def edit_product(id):
+    
+    product=Product.query.filter(Product.id==id).first_or_404()
+
+    if request.method=="GET":
+
+      return render_template ('admin/edit-product.html',product=product)
+    else:
+       name=request.form.get('name',None)
+       price=request.form.get('price',None)
+       description=request.form.get('description',None)
+       active=request.form.get('active',None)
+
+       product.name=name
+       product.price=price
+       product.description=description
+       product.active=active
+
+  
+       if product.active==None:
+          product.active=0 
+       else:
+          product.active=1
+          
+          db.session.commit()
+          return redirect(url_for('admin.edit_product',id=id))
