ce5b6e0f6dbd460d5473fb0eae44b1ca2813dc65
maryam najafi <maryam.najafi733@.com>
2026-08-09 21:53:25 +0330

    general bluprint added

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from bluprints.general import app as general
+
 app=Flask(__name__)
+app.register_blueprint(general)
 
-@app.route("/")
-
-def hello_word():
-    return 'hello'
 
 if __name__=="__main__":
         app.run()
